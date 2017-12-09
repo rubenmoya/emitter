af3a8e39ff8cc79b8c595e4c91fcaa32ae4b6bce
rubenmoya <rmoyarodriguez@gmail.com>
2017-12-09 22:42:06 +0100

    Create Emitter

---
+++ src/emitter.ts
@@ -0,0 +1,24 @@
+type Callback = (event?: any) => void;
+
+export default class Emitter {
+  listeners: Map<string, Callback[]>;
+
+  constructor() {
+    this.listeners = new Map();
+  }
+
+  on(type: string, callback: Callback) {
+    const listeners = this.listeners.has(type) && this.listeners.get(type);
+    listeners ? listeners.push(callback) : this.listeners.set(type, [callback]);
+  }
+
+  off(type: string, callback: Callback) {
+    const listeners = this.listeners.get(type);
+    listeners && listeners.splice(listeners.indexOf(callback) >>> 0, 1);
+  }
+
+  emit(type: string, payload: any) {
+    const listeners = this.listeners.has(type) && this.listeners.get(type);
+    listeners && listeners.forEach(callback => callback(payload));
+  }
+}
