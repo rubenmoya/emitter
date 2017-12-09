6fa44b3908a746f6bd12432f5f04727eac8d6832
rubenmoya <rmoyarodriguez@gmail.com>
2017-12-09 22:42:13 +0100

    Add tests for Emitter

---
+++ src/emitter.test.ts
@@ -0,0 +1,76 @@
+import Emitter from './emitter';
+
+describe('Emitter', () => {
+  let emit: any;
+
+  beforeEach(() => {
+    emit = new Emitter();
+  });
+
+  describe('.on', () => {
+    it('is a function', () => {
+      expect(emit.on).toBeDefined();
+      expect(typeof emit.on).toEqual('function');
+    });
+
+    it('registers handler for new type', () => {
+      const morty = () => {};
+      emit.on('rick', morty);
+
+      expect(emit.listeners.has('rick')).toBeTruthy();
+      expect(emit.listeners.get('rick')).toEqual([morty]);
+    });
+
+    it('registers handlers for any type strings', () => {
+      const jerry = () => {};
+      emit.on('constructor', jerry);
+
+      expect(emit.listeners.has('constructor')).toBeTruthy();
+      expect(emit.listeners.get('constructor')).toEqual([jerry])
+    });
+
+    it('appends handler for existing type', () => {
+      const rick = () => {};
+      const morty = () => {};
+      emit.on('boom', rick)
+      emit.on('boom', morty)
+
+      expect(emit.listeners.has('boom')).toBeTruthy();
+      expect(emit.listeners.get('boom')).toEqual([rick, morty]);
+    });
+  });
+
+  describe('.off', () => {
+    it('is a function', () => {
+      expect(emit.off).toBeDefined()
+      expect(typeof emit.off).toEqual('function')
+    })
+
+    it('removes handler for type', () => {
+      const rick = () => {};
+      emit.on('pickle', rick);
+      emit.off('pickle', rick);
+
+      expect(emit.listeners.has('pickle')).toBeTruthy();
+      expect(emit.listeners.get('pickle')).toEqual([]);
+    });
+  });
+
+  describe('.emit', () => {
+    it('is a function', () => {
+      expect(emit.emit).toBeDefined()
+      expect(typeof emit.emit).toEqual('function')
+    })
+
+    it('invokes handler for type', () => {
+      const jerry = { mr: 'meeseeks' };
+
+      emit.on('pickle', (one: any, two: any) => {
+        expect(one).toEqual(jerry);
+        expect(two).toBeUndefined();
+      });
+
+      emit.emit('pickle', jerry);
+    });
+  });
+});
